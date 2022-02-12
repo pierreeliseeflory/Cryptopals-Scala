626bd5f5beb6422ca9db62d472ff3bbafe1fea06
pierreeliseeflory <pierre-elisee.flory@ensta-paris.fr>
2022-02-12 14:03:55 +0100

    update challenge 1

--- set_1/challenge_1/src/main/scala/Main.scala
+++ set_1/challenge_1/src/main/scala/Main.scala
@@ -6,21 +6,25 @@ def ASCIIToHexa = (hex: Char) => hex.toInt match {
     case _ => throw new Exception("Incorrect hex character")
 }
 
-def Base64ToASCII = (value: Int) => value match {
-    case value if (value >= 0 && value < 26) => value + 65
-    case value if (value >= 26 && value < 52) => value + 71
-    case value if (value >= 52 && value < 62) => value - 4 
-    case 62 => 43
-    case 63 => 47
-    case _ => throw new Exception("Incorrect value : 0 <= Base 64 < 64")
+def Base64ToASCII (value: Int, padding: Boolean = false) = 
+    if padding then 
+        61
+    else
+        value match {
+            case value if (value >= 0 && value < 26) => value + 65
+            case value if (value >= 26 && value < 52) => value + 71
+            case value if (value >= 52 && value < 62) => value - 4 
+            case 62 => 43
+            case 63 => 47
+            case _ => throw new Exception("Incorrect value : 0 <= Base 64 < 64")
 }
 
-def hexaTripletToBase64Couple(value_1: Int, value_2: Int, value_3: Int) = {
-    (Base64ToASCII((value_1 << 2) + (value_2 / 4)), Base64ToASCII(((value_2 % 4) << 4) + value_3))
+def hexaTripletToBase64Couple(value_1: Int, value_2: Int, value_3: Int, padding: Boolean) : (Int, Int)  = {
+    (Base64ToASCII((value_1 << 2) + (value_2 / 4)), Base64ToASCII(((value_2 % 4) << 4) + value_3, padding))
 }
 
-def hexaToBase64(value_1: Char, value_2: Char = '0', value_3: Char = '0') : (Int, Int) = {
-    hexaTripletToBase64Couple(ASCIIToHexa(value_1), ASCIIToHexa(value_2), ASCIIToHexa(value_3))
+def hexaToBase64(value_1: Char, value_2: Char = '0', value_3: Char = '0', padding: Boolean = false) : (Int, Int) = {
+    hexaTripletToBase64Couple(ASCIIToHexa(value_1), ASCIIToHexa(value_2), ASCIIToHexa(value_3), padding)
 }
 
 def iterateOverList(inputList: List[Char]) : List[Int] = inputList match {
@@ -28,29 +32,23 @@ def iterateOverList(inputList: List[Char]) : List[Int] = inputList match {
         val (res_1, res_2) = hexaToBase64(x, y, z)
         res_1 :: res_2 :: iterateOverList(rest)
     }
-    case x :: y :: rest => {
-        println("2")
+    case x :: y :: Nil => {
         val (res_1, res_2) = hexaToBase64(x, y)
-        res_1 :: res_2 :: iterateOverList(rest)
+        res_1 :: res_2 :: List() 
     }
-    case x :: rest => {
-        println("1")
-        val (res_1, res_2) = hexaToBase64(x)
-        res_1 :: res_2 :: iterateOverList(rest)
+    case x :: Nil => {
+        val (res_1, res_2) = hexaToBase64(x, padding=true)
+        res_1 :: res_2 :: List() 
     }
-    case _ => {
+    case Nil  => {
         List()
     }
 }
 
 @main
 def Challenge() : Unit =
-    val input = readLine()
-    val list_chars = input.toList
+    val input = readLine().toList
 
-    val result = iterateOverList(list_chars)
-
-    for char <- result do print(char.toChar) 
+    for char <- iterateOverList(input) do print(char.toChar) 
     println
-
 end Challenge
